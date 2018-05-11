a647ec244bb76434d31ed6d81dabccf4639968d5
jatin1998nair <35593920+jatin1998nair@users.noreply.github.com>
2018-05-11 12:16:32 +0530

    Add files via upload

--- jan.sh
+++ jan.sh
@@ -1,4 +1,4 @@
-rsync -avzh Documents back
+rsync -avzh Downloads back
 notify-send -t 900 "notification" "backup of documents is done"" $(date +%r)"
 #!/bin/bash
 if (whiptail --title "anyother files needs updation?" --yesno "Choose between Yes and No." 10 60) 
@@ -10,19 +10,14 @@ fi
 
 dialog --title "backup" --msgbox "backed up date " $(date +%r) 10 50
 
-dialog --title "backup" --msgbox "if any other files needed to be backed up? (y/n)" 10 50
+dialog --title "confirmation" --msgbox "are you sure that if any other files needed to be backed up? (y/n)" 10 50
 read x
 if [ $x = "y" ]
 then 	
 dialog --title "backup" --msgbox "enter the filename to be backed up?" 10 50
 read u
 rsync -avzh $u back
-if [ $u ne -f ]
-then
-dialog --title "backup" --msgbox "the file does not exist"
-else
 dialog --title "backup notification" --msgbox "$u has been backed up in backup folder""$(date +%r)" 10 50
-fi
 else
 exit
 fi
